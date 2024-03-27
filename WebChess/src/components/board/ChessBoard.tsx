fc3e9f8ccf75ddf33197127b4ce64a2af8427ed3
Milan Rasche <theslipperytrout@gmail.com>
2024-03-27 21:05:51 +0100

    added logic to castle still need to add logic for when castling is no longer allowed

--- WebChess/src/components/board/ChessBoard.tsx
+++ WebChess/src/components/board/ChessBoard.tsx
@@ -15,12 +15,6 @@ import { isTurn } from '../rules/IsTurn';
 const ChessBoard: React.FC = () => {
 
     const [pieces, setPieces] = useState<Piece[]>([]);
-    const [castlingRights, setCastlingRights] = useState({
-        whiteShort: true,
-        whiteLong: true,
-        blackShort: true,
-        blackLong: true
-    });
     const params = useParams();
 
     useEffect(() => {
@@ -73,14 +67,34 @@ const ChessBoard: React.FC = () => {
     function castleKingSide(y: number, prevPieces: Piece[]): boolean {
         const kingSidePieceInWay = prevPieces.find(piece =>
             piece.position.x === 5 && piece.position.y === y);
-        if (kingSidePieceInWay) {
+        const kingSidePieceInWayTwo = prevPieces.find(piece =>
+            piece.position.x === 6 && piece.position.y === y);
+        if (kingSidePieceInWay || kingSidePieceInWayTwo) {
             return false;
         }
         return true;
     }
 
-    function castleQueenSide() {
+    function castleQueenSide(y: number, prevPieces: Piece[]): boolean {
+        const kingSidePieceInWay = prevPieces.find(piece =>
+            piece.position.x === 1 && piece.position.y === y);
+        const kingSidePieceInWayTwo = prevPieces.find(piece =>
+            piece.position.x === 2 && piece.position.y === y);
+        const kingSidePieceInWayThree = prevPieces.find(piece =>
+            piece.position.x === 3 && piece.position.y === y);
+        if (kingSidePieceInWay || kingSidePieceInWayTwo || kingSidePieceInWayThree) {
+            return false;
+        }
+        return true;
+    }
 
+    function toggleTurn() {
+        let currentPlayerColor = sessionStorage.getItem('turn');
+        if (currentPlayerColor === 'white') {
+            sessionStorage.setItem('turn', 'black');
+        } else {
+            sessionStorage.setItem('turn', 'white');
+        }
     }
 
     const handleDrop = (fromPosition: Position, toPosition: Position) => {
@@ -115,24 +129,63 @@ const ChessBoard: React.FC = () => {
             let castling = sessionStorage.getItem("castling")
             if (castling?.includes("K") && pieceFromPosition.type === PieceType.KingWhite && (toPosition.y === 7 && toPosition.x === 6)) {
                 if(castleKingSide(7, prevPieces)){
-                    let kingMoved = prevPieces.map(piece => {
+                    let castledWhiteKingSide = prevPieces.map(piece => {
                         if (piece.position.x === fromPosition.x && piece.position.y === fromPosition.y) {
                             return { ...piece, position: { ...toPosition } }; // Create a new object with updated position
+                        } else if (piece.position.x === 7 && piece.position.y === 7) {
+                            // Handle moving the rook
+                            return { ...piece, position: { x: 5, y: 7 } }; // Assuming the rook's new position after castling is (5, 7)
                         }
                         return piece;
                     });
+                    toggleTurn();
+                    return castledWhiteKingSide;
                 }
             }
             else if (castling?.includes("k") && pieceFromPosition.type === PieceType.KingBlack && (toPosition.y === 0 && toPosition.x === 6)) {
                 if (castleKingSide(0, prevPieces)) {
-
+                    let castledBlackKingSide = prevPieces.map(piece => {
+                        if (piece.position.x === fromPosition.x && piece.position.y === fromPosition.y) {
+                            return { ...piece, position: { ...toPosition } }; // Create a new object with updated position
+                        } else if (piece.position.x === 7 && piece.position.y === 0) {
+                            // Handle moving the rook
+                            return { ...piece, position: { x: 5, y: 0 } }; // Assuming the rook's new position after castling is (5, 7)
+                        }
+                        return piece;
+                    });
+                    toggleTurn();
+                    return castledBlackKingSide;
                 }
             }
             else if (castling?.includes("Q") && pieceFromPosition.type === PieceType.KingWhite && (toPosition.y === 7 && toPosition.x === 2)) {
-
+                if (castleQueenSide(7, prevPieces)) {
+                    let castledWhiteQueenSide = prevPieces.map(piece => {
+                        if (piece.position.x === fromPosition.x && piece.position.y === fromPosition.y) {
+                            return { ...piece, position: { ...toPosition } }; // Create a new object with updated position
+                        } else if (piece.position.x === 0 && piece.position.y === 7) {
+                            // Handle moving the rook
+                            return { ...piece, position: { x: 3, y: 7 } }; // Assuming the rook's new position after castling is (5, 7)
+                        }
+                        return piece;
+                    });
+                    toggleTurn();
+                    return castledWhiteQueenSide;
+                }
             }
             else if (castling?.includes("q") && pieceFromPosition.type === PieceType.KingBlack && (toPosition.y === 0 && toPosition.x === 2)){
-
+                if (castleQueenSide(0, prevPieces)) {
+                    let castledBlackQueenSide = prevPieces.map(piece => {
+                        if (piece.position.x === fromPosition.x && piece.position.y === fromPosition.y) {
+                            return { ...piece, position: { ...toPosition } }; // Create a new object with updated position
+                        } else if (piece.position.x === 0 && piece.position.y === 0) {
+                            // Handle moving the rook
+                            return { ...piece, position: { x: 3, y: 0 } }; // Assuming the rook's new position after castling is (5, 7)
+                        }
+                        return piece;
+                    });
+                    toggleTurn();
+                    return castledBlackQueenSide;
+                }
             }
 
             if (!isPieceMovementLegal(pieceFromPosition, fromPosition, toPosition, prevPieces)) {
@@ -160,12 +213,7 @@ const ChessBoard: React.FC = () => {
                 return prevPieces;
             }
 
-            if (!isLowerCase(pieceFromPosition.type)){
-                sessionStorage.setItem("turn", "black")
-            }
-            else {
-                sessionStorage.setItem("turn", "white")
-            }
+            toggleTurn();
 
             return updatedPieces;
         });
