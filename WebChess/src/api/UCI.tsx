24e6a45193363b46e97f84c7c98c2be8d6f88f62
Milan Rasche <theslipperytrout@gmail.com>
2024-05-20 22:18:38 +0200

    attempted fix

--- WebChess/src/api/UCI.tsx
+++ WebChess/src/api/UCI.tsx
@@ -1,23 +1,39 @@
+import axios from "axios";
 
 const url = 'https://localhost:5165/api/Chess'
 
+interface TextResponse {
+    text: string;
+}
+
 export async function startNewGame(): Promise<string | null> {
-    
-    try {
-        const response = await fetch(`${url}?message=uci`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+    // const text = "uci"
+    // try {
+    //     const response = await axios.post<TextResponse>(
+    //         'https://localhost:5165/api/Chess/text', // Replace with your actual API URL
+    //         { text }
+    //     );
+    //     console.log( response.data.text);
+    //     return response.data.text
+    // } catch (error) {
+    //     console.error(error);
+    //     return null
+    // }
+    fetch('https://localhost:7053/api/UCI', { method: "GET" }) // Construct url
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error('Network response was not ok ' + res.statusText);
+            }
+        })
+        .then((data) => {
+            console.log(data);
+            return data
+        })
+        .catch((error) => {
+            console.error('There was a problem with the fetch operation:', error);
         });
-        
-        if (!response.ok) { console.log(response)}
-        else {
-            console.log(await response.text());
-        }
-    }
-    catch (error) {
-        throw new Error(`Something went wrong: ${error.message}`);
-    }
-    return null;
+
+    return null
 }
 
 // async function startNewGameaFEN(message: string): Promise<string | null> {
